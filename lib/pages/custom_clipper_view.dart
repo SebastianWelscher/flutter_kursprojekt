e2eb3460cea4e15f41eae003bac6856e5a148166
Sebastian Welscher <SebastianWelscher@mailbox.org>
2024-05-20 11:44:29 +0200

    update custom_clipper_view

---
+++ lib/pages/custom_clipper_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CustomClipperView extends StatelessWidget {
+  const CustomClipperView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
